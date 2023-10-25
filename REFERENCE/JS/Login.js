85213b0a35b6a4414c03ce4c8d7bb10bb1fa0a17
Nathanael Memis <nathanaelmemis@gmail.com>
2023-10-26 00:36:25 +0800

    404.html changes

--- REFERENCE/JS/Login.js
+++
@@ -1,8 +0,0 @@
-console.log ("Javascript Active!");
-
-function wrongInput()
-{
-    console.log ("wrongInput");
-    document.getElementById("main-login-error-container").style.display = "block";
-}
-
